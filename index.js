af84ac8d5af56527b30c52d8d0dd7ffd09f356bc
nhan055 <97578062+HTNhan0505@users.noreply.github.com>
2022-11-16 12:00:31 +0700

    setupCLI

--- index.js
+++ index.js
@@ -9,6 +9,59 @@ const app = express()
 const awsCLIS3 = {}
 app.use(cors())
 app.use(express.json())
+// config aws s3 
+const s3 = new S3({
+	region:awsCLIS3.region,
+	accessKeyId:awsCLIS3.accessKeyId,
+	secretAccessKey:awsCLIS3.secretAccessKey,
+	sessionToken:awsCLIS3.sessionToken
+})
+// upload anh len aws s3
+function uploadFile(file){
+    const fileStream = fs.createReadStream(file.path)
+    const uploadParams={
+        Bucket:'rekognitionimagesss',
+        Body:fileStream,
+        Key:file.originalname
+    }
+    return s3.upload(uploadParams).promise()
+}
+// tro toi folder img
+const upload = multer({dest:'img/'})
+// test port xem da connect postman
+app.get('/api',(req,res)=>{
+
+    res.status(200).send('Connect!!!!!')
+})
+// port de ket noi aws CLI
+app.post('/api/setCLI', (req,res)=>{
+	AWS.config.credentials.accessKeyId = req.body.accessKeyId
+	AWS.config.credentials.secretAccessKey = req.body.secretAccessKey
+	AWS.config.credentials.sessionToken = req.body.sessionToken
+	AWS.config.region = 'us-east-1'
+	
+	awsCLIS3.accessKeyId = req.body.accessKeyId
+	awsCLIS3.secretAccessKey = req.body.secretAccessKey
+	awsCLIS3.sessionToken = req.body.sessionToken
+	awsCLIS3.region = 'us-east-1'
+
+
+	AWS.config.update({
+		accessKeyId:req.body.accessKeyId,
+		secretAccessKey:req.body.secretAccessKey,
+		sessionToken:req.body.sessionToken,
+		region:'us-east-1',
+		// signatureVersion: 'v4'
+	})
+	new AWS.S3({
+		accessKeyId:req.body.accessKeyId,
+		secretAccessKey:req.body.secretAccessKey,
+		sessionToken:req.body.sessionToken,
+		region:'us-east-1',
+	})
+	console.log(AWS.config);
+	res.send('Ket noi thanh cong')
+})
 
 // kiểm tra xem app có chạy đúng port 5000 hay không
 app.listen(5000,()=>{
